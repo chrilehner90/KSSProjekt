22726d2ea100eb4153257e4454ec1d32147b8e6b
Christian Lehner <lehner.chri@gmail.com>
2015-04-17 11:02:06 +0200

    added a basic socket.io connection

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-var http = require('http');
+var app = require('http').createServer(handler);
 var fs = require('fs');
-var io = require('socket.io');
+var io = require('socket.io')(app);
+
+app.listen(3000, function() {
+	console.log("Listening on port 3000");
+});
 
 
-
-fs.readFile('./index.html', function(err, html) {
-	if(err) console.log(err);
-	else {
-		var server = http.createServer(function(req, res) {
+function handler(req, res) {
+	fs.readFile('./index.html', function(err, html) {
+		if(err) console.log(err);
+		else {
 			console.log("Client connected");
 
 			res.writeHead(200, {'Content-Type': 'text/html'});
 			res.write(html);
 			res.end();
-		});
 
-		server.listen(3000, function() {
-			console.log("Listening on port 3000");
-		});
-	}
-});
+			
 
-
-
-
+			io.on('connection', function(socket) {
+				socket.emit('hello', { msg: 'hello world!' });
+			});
+		}
+	});
+}
