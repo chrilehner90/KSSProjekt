0c0d73a424218438a04722add85e9158e2b76b30
Andreas Frank <afrank.mmt-m2014@fh-salzburg.ac.at>
2015-05-31 14:49:03 +0200

    update cpp file

--- Measurement/main.cpp
+++ Measurement/main.cpp
@@ -40,6 +40,25 @@ double corrected_sample_standard_deviation(T* data, uint32_t elem) {
 }
 
 
+// measure time for x clients
+double testForXClients(int n) {
+
+    string filePath = "http://localhost:3000";
+    string command = "open " + filePath;
+
+    auto start = chrono::high_resolution_clock::now();    
+    
+    for(int x = 0; x < n; x++)
+    {
+        system(command.c_str());
+    }
+
+    double time_in_microseconds = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count();
+
+    return time_in_microseconds;
+}
+
+
 int main() {
 
     /*
@@ -48,57 +67,30 @@ int main() {
     */
 
     int repititions = 5;
-    
 
-    //clock_t begin;
-    //clock_t end;
-
+    double samplesTest[repititions];
         
 
-        // measure time for computing KMP and Naive with search-string lengths from 10 to 100
+        // measure time for 
         for(int clients = 2; clients <= 2; clients += 10)
         {
 
             // calculate the runtime for 5 repititions
             for(int j = 0; j < repititions; j++)
             {
-
-                for(int x = 0; x < clients; x++)
-                {
-                    /*string filePath = "http://localhost:3000";
-                    string command = "open " + filePath;
-                    system(command.c_str());
-                    */
-                }
-
-
-                /*begin = clock();
-                KMP_Search(searchText, searchString);
-                end = clock();
-
-                neededTimeKMP = neededTimeKMP + (double(end - begin) / CLOCKS_PER_SEC);
-
-                begin = clock();
-                Naive_Search(searchText, searchString);
-                end = clock();
-
-                neededTimeNaive = neededTimeNaive + (double(end - begin) / CLOCKS_PER_SEC);
-                */
+                    double test = testForXClients(clients);
+                    samplesTest[j] = test; 
             }
 /*
             // save the results in a file
-            fprintf(f, "lst\t%d", (int)searchString.length());
-            fprintf(f, "\tt_KMP\t%f", (neededTimeKMP / repititions));
-            fprintf(f, "\tt_Naive\t%f", (neededTimeNaive / repititions));
-            fprintf(f, "\tcount_KMP\t%f", countKMP);
-            fprintf(f, "\tcount_Naive\t%f\n", countNaive);
+            fprintf(f, "clients\t%d", clients);
+            fprintf(f, "\mean\t%d", arithmetic_mean(samplesIterativ, repititions));
+            fprintf(f, "\sd\t%f", corrected_sample_standard_deviation(samplesIterativ, repititions));
 
-            neededTimeKMP = 0;
-            neededTimeNaive = 0;
             */
         }
 
-        //cout << arithmetic_mean(samplesIterativ, repititions) << endl;
+        cout << arithmetic_mean(samplesTest, repititions) << endl;
         //cout << corrected_sample_standard_deviation(samplesIterativ, repititions) << endl;
 
         //fclose(f);
